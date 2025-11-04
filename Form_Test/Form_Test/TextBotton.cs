44ce4105b4275190db56a547dcd20196bc50086f
H_YOKOYAMA\yohir <yohiro1011@gmail.com>
2025-11-04 11:39:29 +0900

    システム

--- Form_Test/Form_Test/TextBotton.cs
+++ Form_Test/Form_Test/TextBotton.cs
@@ -66,7 +66,11 @@ namespace Form_Test
         // 自分で作成することも可能
         private void ClickEvent(object sender, EventArgs e)
         {
-            _form1.GetTestButton(_x,_y).Toggle();
+            _form1.GetTestButton(_x, _y)?.Toggle();
+            _form1.GetTestButton(_x+1, _y)?.Toggle();
+            _form1.GetTestButton(_x-1, _y)?.Toggle();
+            _form1.GetTestButton(_x, _y+1)?.Toggle();
+            _form1.GetTestButton(_x, _y-1)?.Toggle();
         }
     }
 }
